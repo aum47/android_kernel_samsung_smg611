45924ecfc77cad1d28fdf62b0ff930951c41d439
javilonas <javilonas@esp-desarrolladores.com>
2017-01-23 15:37:49 +0100

    zswap: use LZ4 if available
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -106,9 +106,12 @@ static bool zswap_enabled __read_mostly = 1;
 module_param_named(enabled, zswap_enabled, bool, 0444);
 
 /* Compressor to be used by zswap (fixed at boot for now) */
+#ifdef CONFIG_CRYPTO_LZ4
+#define ZSWAP_COMPRESSOR_DEFAULT "lz4"
+#else
 #define ZSWAP_COMPRESSOR_DEFAULT "lzo"
-#define ZSWAP_COMPRESSOR "lz4"
-static char *zswap_compressor = ZSWAP_COMPRESSOR;
+#endif
+static char *zswap_compressor = ZSWAP_COMPRESSOR_DEFAULT;
 module_param_named(compressor, zswap_compressor, charp, 0444);
 
 /* The maximum rate (1/1000) of memory that the compressed pool can occupy */
