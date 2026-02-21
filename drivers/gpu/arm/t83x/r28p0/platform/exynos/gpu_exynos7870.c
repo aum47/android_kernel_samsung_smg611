249287404c3bcb66208b9021c7bdc01772e7cb75
Andevoloper <yogurtbirsah65@gmail.com>
2026-02-22 00:59:56 +0300

    drivers: gpu: r28p0: Add GPU Freq and Volt control

--- drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_exynos7870.c
+++ drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_exynos7870.c
@@ -97,6 +97,7 @@ static gpu_attribute gpu_config_attributes[] = {
 	{GPU_MAX_CLOCK, 1001},
 	{GPU_MAX_CLOCK_LIMIT, 1001},
 	{GPU_MIN_CLOCK, 343},
+	{GPU_MIN_CLOCK_LIMIT, 343},
 	{GPU_DVFS_START_CLOCK, 343},
 	{GPU_DVFS_BL_CONFIG_CLOCK, 343},
 	{GPU_GOVERNOR_TYPE, G3D_DVFS_GOVERNOR_INTERACTIVE},
@@ -486,12 +487,13 @@ int gpu_enable_dvs(struct exynos_context *platform)
 #ifdef CONFIG_EXYNOS_CL_DVFS_G3D
 	if (!platform->dvs_is_enabled) {
 		if (platform->exynos_pm_domain) {
-		mutex_lock(&platform->exynos_pm_domain->access_lock);
-		if (!platform->dvs_is_enabled && gpu_is_power_on()) {
-			level = gpu_dvfs_get_level(gpu_get_cur_clock(platform));
-			exynos_cl_dvfs_stop(ID_G3D, level);
+			mutex_lock(&platform->exynos_pm_domain->access_lock);
+			if (!platform->dvs_is_enabled && gpu_is_power_on()) {
+				level = gpu_dvfs_get_level(gpu_get_cur_clock(platform));
+				exynos_cl_dvfs_stop(ID_G3D, level);
+			}
+			mutex_unlock(&platform->exynos_pm_domain->access_lock);
 		}
-		mutex_unlock(&platform->exynos_pm_domain->access_lock);
 	}
 #endif /* CONFIG_EXYNOS_CL_DVFS_G3D */
 
