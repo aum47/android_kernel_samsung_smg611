8fc66e686b2037fcd6b7fe62b13c7930526737e8
aum47 <aum.patel131108@gmail.com>
2026-05-18 08:07:54 +0530

    Pull ReSukiSU commits and un-streamline build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,13 +3,10 @@ export ARCH=arm64
 export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
-mkdir -p out
-
 # Fix yylloc double definition (idempotent)
 grep -q 'extern YYLTYPE yylloc' scripts/dtc/dtc-lexer.l || \
     sed -i 's/YYLTYPE yylloc/extern YYLTYPE yylloc/' scripts/dtc/dtc-lexer.l
 flex -o scripts/dtc/dtc-lexer.lex.c scripts/dtc/dtc-lexer.l
 
-make ARCH=arm64 O=out exynos7870-on7xreflte_defconfig
-make ARCH=arm64 O=out prepare
-make ARCH=arm64 O=out -j64 2>&1 | tee out/build.log
+make exynos7870-on7xreflte_defconfig
+make -j64 2>&1 | tee build.log
