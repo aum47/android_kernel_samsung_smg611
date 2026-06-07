88e9bf5cb93d80f36c271e9f310785357af861bc
aum47 <aum.patel131108@gmail.com>
2026-06-07 21:52:09 +0530

    build script: retire on-the-go yylloc fix

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,10 +3,5 @@ export ARCH=arm64
 export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
-# Fix yylloc double definition (idempotent)
-grep -q 'extern YYLTYPE yylloc' scripts/dtc/dtc-lexer.l || \
-    sed -i 's/YYLTYPE yylloc/extern YYLTYPE yylloc/' scripts/dtc/dtc-lexer.l
-flex -o scripts/dtc/dtc-lexer.lex.c scripts/dtc/dtc-lexer.l
-
 make exynos7870-on7xreflte_defconfig
 make -j64 2>&1 | tee build.log
