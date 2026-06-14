1117edaf4ab4c8b839357cb6b0df562d8ad5e144
aum47 <aum.patel131108@gmail.com>
2026-06-14 21:49:24 +0530

    Update pwrcal and drop undervolt on S5E7870-asv.c
    why? phone refused to boot

--- drivers/soc/samsung/pwrcal/S5E7870/S5E7870-asv.c
+++ drivers/soc/samsung/pwrcal/S5E7870/S5E7870-asv.c
@@ -357,8 +357,8 @@ static void asv_set_freq_limit(void)
 notfused:
 
 #ifdef PWRCAL_TARGET_LINUX
-	asv_dvfs_cpucl0->table->max_freq = 1690000;
-	asv_dvfs_cpucl1->table->max_freq = 1690000;
+	asv_dvfs_cpucl0->table->max_freq = 1586000;
+	asv_dvfs_cpucl1->table->max_freq = 1586000;
 	asv_dvfs_g3d->table->max_freq = 1246000;
 	asv_dvfs_mif->table->max_freq = 900000;
 #endif
@@ -636,8 +636,8 @@ static int dvfscpucl0_get_asv_table(unsigned int *table)
 	Define Needed Voltages manually */
 	table[0] = 1200000;
 	table[1] = 1375000;
-	table[2] = 1000000;
-	table[3] = 975000;
+	table[2] = 1150000;
+	table[3] = 1000000;
 	table[4] = 950000;
 	table[5] = 900000;
 	table[6] = 850000;
@@ -668,8 +668,8 @@ static int dvfscpucl1_get_asv_table(unsigned int *table)
 	Define Needed Voltages manually */
 	table[0] = 1200000;
 	table[1] = 1325000;
-	table[2] = 1000000;
-	table[3] = 975000;
+	table[2] = 1150000;
+	table[3] = 1000000;
 	table[4] = 950000;
 	table[5] = 900000;
 	table[6] = 850000;
