5ba9872ef30826b74090c03d60f216d857ab14da
aum47 <aum.patel131108@gmail.com>
2026-06-27 09:45:21 +0530

    Sync build_kernel.sh from main

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 export ARCH=arm64
 export CROSS_COMPILE=/home/aumpatel/toolchain/gcc-linaro-4.9/bin/aarch64-linux-gnu-
 export ANDROID_MAJOR_VERSION=p
 
 make exynos7870-on7xreflte_defconfig
-make -j$(nproc) 2>&1 | tee build.log
+make -j64 2>&1 | tee build.log
