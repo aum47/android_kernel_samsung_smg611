37800fd089a0a787a766ec2d32189678928850b6
aum47 <aum.patel131108@gmail.com>
2026-06-27 09:46:11 +0530

    ovl: fix ovl_follow_link for old 3.18 nameidata-based symlink API
    ovl_follow_link was written against the new cookie-based VFS symlink
    API (void **cookie, const char * return) which does not exist on
    3.18. Rewrite it to use the old API: take struct nameidata * as the
    second argument, return void * cookie, and pass nd through to the
    real filesystem's follow_link so it can call nd_set_link internally.
    ovl_put_link already had the correct 3.18 signature.

--- fs/overlayfs/inode.c
+++ fs/overlayfs/inode.c
@@ -137,38 +137,36 @@ struct ovl_link_data {
 	void *cookie;
 };
 
-static const char *ovl_follow_link(struct dentry *dentry, void **cookie)
+static void *ovl_follow_link(struct dentry *dentry, struct nameidata *nd)
 {
-	struct dentry *realdentry;
-	struct inode *realinode;
-	struct ovl_link_data *data = NULL;
-	const char *ret;
+    struct dentry *realdentry;
+    struct inode *realinode;
+    struct ovl_link_data *data = NULL;
+    void *cookie;
 
-	realdentry = ovl_dentry_real(dentry);
-	realinode = realdentry->d_inode;
+    realdentry = ovl_dentry_real(dentry);
+    realinode = realdentry->d_inode;
 
-	if (WARN_ON(!realinode->i_op->follow_link))
-		return ERR_PTR(-EPERM);
+    if (WARN_ON(!realinode->i_op->follow_link))
+        return ERR_PTR(-EPERM);
 
-	if (realinode->i_op->put_link) {
-		data = kmalloc(sizeof(struct ovl_link_data), GFP_KERNEL);
-		if (!data)
-			return ERR_PTR(-ENOMEM);
-		data->realdentry = realdentry;
-	}
+    if (realinode->i_op->put_link) {
+        data = kmalloc(sizeof(struct ovl_link_data), GFP_KERNEL);
+        if (!data)
+            return ERR_PTR(-ENOMEM);
+        data->realdentry = realdentry;
+    }
 
-	ret = realinode->i_op->follow_link(realdentry, cookie);
-	if (IS_ERR_OR_NULL(ret)) {
-		kfree(data);
-		return ret;
-	}
+    cookie = realinode->i_op->follow_link(realdentry, nd);
+    if (IS_ERR_OR_NULL(cookie)) {
+        kfree(data);
+        return cookie;
+    }
 
-	if (data)
-		data->cookie = *cookie;
+    if (data)
+        data->cookie = cookie;
 
-	*cookie = data;
-
-	return ret;
+    return data ? (void *)data : cookie;
 }
 
 static void ovl_put_link(struct dentry *dentry,
